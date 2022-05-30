b24f525c051e042d5ee924864d6a4704f9b0f234
Suchanon Aegkathat <70258649+samcarindar@users.noreply.github.com>
2022-05-31 00:23:05 +0700

    update

--- models/UserAddressModel.js
+++ models/UserAddressModel.js
@@ -11,25 +11,26 @@ const userAddressSchema = mongoose.Schema(
     userAddress: {
       amphure: {
         AMPHUR_ID: { type: Number },
-        AMPHUR_NAME: { type: String }
+        AMPHUR_NAME: { type: String },
       },
       district: {
         DISTRICT_ID: { type: String },
-        DISTRICT_NAME: { type: String }
+        DISTRICT_NAME: { type: String },
       },
       province: {
         PROVINCE_ID: { type: Number },
-        PROVINCE_NAME: { type: String }
+        PROVINCE_NAME: { type: String },
       },
       your_address: { type: String },
-      zip_code: { type: String }
+      zip_code: { type: String },
     },
     addressDefault: {
+      addressShipping: { type: Number },
       addressOrderDefault: { type: String },
       addressKatecheckDefault: { type: String },
       addressSellWithUsDefault: { type: String },
       addressReturnProductDefault: { type: String },
-    }
+    },
   },
   {
     timestamps: true,
--- routers/userAddressRoute.js
+++ routers/userAddressRoute.js
@@ -16,9 +16,100 @@ userAddressRoute.get("/", Auth, async (req, res) => {
 userAddressRoute.get("/:id", Auth, async (req, res) => {
   try {
     const { id } = req.params;
+    const result = await UserAddress.find({ "user.user_id": id });
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
-    // let userAddress = await UserAddress.find();
-    const result = await UserAddress.find({ 'user.user_id': id });
+userAddressRoute.get("/edit-address/:id", Auth, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
+    }
+
+    const result = await UserAddress.findOne({ _id: id });
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+userAddressRoute.get("/delete-address/:id", Auth, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
+    }
+
+    const result = await UserAddress.deleteOne({ _id: id });
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+userAddressRoute.put("/:id", Auth, async (req, res) => {
+  try {
+    let { data } = req.fields;
+    data = JSON.parse(data);
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
+    }
+
+    const result = await UserAddress.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: data,
+      },
+      {
+        upsert: true,
+        returnDocument: "after", // this is new !
+      }
+    );
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+userAddressRoute.put("/activete-address/:id", Auth, async (req, res) => {
+  try {
+    let { data } = req.fields;
+    data = JSON.parse(data);
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
+    }
+
+    await UserAddress.updateMany(
+      { "user.user_id": data.user.user_id },
+      {
+        $set: { "addressDefault.addressShipping": null },
+      },
+      {
+        upsert: true,
+        returnDocument: "after", // this is new !
+      }
+    );
+
+    const result = await UserAddress.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: data,
+      },
+      {
+        upsert: true,
+        returnDocument: "after", // this is new !
+      }
+    );
     res.json(result);
   } catch (error) {
     res.json(error);
@@ -30,6 +121,7 @@ userAddressRoute.post("/", async (req, res) => {
     let { data } = req.fields;
     data = JSON.parse(data);
     const result = await UserAddress.create(data);
+    updateOne({ _id: 1, grades: 80 }, { $set: { "grades.$": 82 } });
     res.json(result);
   } catch (error) {
     res.json(error);
