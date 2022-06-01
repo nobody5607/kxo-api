156ce5e70cba5669e5d014fc1b7c02e20ef66837
samcarindar <samcarindar1@gmail.com>
2022-06-01 16:25:52 +0700

    update

--- models/UserAddressModel.js
+++ models/UserAddressModel.js
@@ -1,35 +1,72 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 const userAddressSchema = mongoose.Schema(
+  // {
+  //   user: {
+  //     user_id: { type: String },
+  //     user_name: { type: String },
+  //     email: { type: String },
+  //     phone: { type: String },
+  //   },
+  //   userAddress: {
+  //     amphure: {
+  //       AMPHUR_ID: { type: Number },
+  //       AMPHUR_NAME: { type: String },
+  //     },
+  //     district: {
+  //       DISTRICT_ID: { type: String },
+  //       DISTRICT_NAME: { type: String },
+  //     },
+  //     province: {
+  //       PROVINCE_ID: { type: Number },
+  //       PROVINCE_NAME: { type: String },
+  //     },
+  //     your_address: { type: String },
+  //     zip_code: { type: String },
+  //   },
+  //   addressDefault: {
+  //     addressShipping: { type: Number },
+  //     addressOrderDefault: { type: String },
+  //     addressKatecheckDefault: { type: String },
+  //     addressSellWithUsDefault: { type: String },
+  //     addressReturnProductDefault: { type: String },
+  //   },
+  // },
   {
-    user: {
-      user_id: { type: String },
-      user_name: { type: String },
-      email: { type: String },
-      phone: { type: String },
-    },
     userAddress: {
-      amphure: {
-        AMPHUR_ID: { type: Number },
-        AMPHUR_NAME: { type: String },
+      user: {
+        user_id: { type: String },
+        user_name: { type: String },
+        email: { type: String },
+        phone: { type: String },
       },
-      district: {
-        DISTRICT_ID: { type: String },
-        DISTRICT_NAME: { type: String },
+      address: {
+        address1: {
+          userAddr: {
+            amphure: {
+              AMPHUR_ID: { type: Number },
+              AMPHUR_NAME: { type: String },
+            },
+            district: {
+              DISTRICT_ID: { type: String },
+              DISTRICT_NAME: { type: String },
+            },
+            province: {
+              PROVINCE_ID: { type: Number },
+              PROVINCE_NAME: { type: String },
+            },
+            your_address: { type: String },
+            zip_code: { type: String },
+          },
+        },
       },
-      province: {
-        PROVINCE_ID: { type: Number },
-        PROVINCE_NAME: { type: String },
+      addressDefault: {
+        addressShipping: { type: Number },
+        addressOrderDefault: { type: Number },
+        addressKatecheckDefault: { type: Number },
+        addressSellWithUsDefault: { type: Number },
+        addressReturnProductDefault: { type: Number },
       },
-      your_address: { type: String },
-      zip_code: { type: String },
-    },
-    addressDefault: {
-      addressShipping: { type: Number },
-      addressOrderDefault: { type: String },
-      addressKatecheckDefault: { type: String },
-      addressSellWithUsDefault: { type: String },
-      addressReturnProductDefault: { type: String },
     },
   },
   {
