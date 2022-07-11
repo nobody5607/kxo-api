412c39aa714fd698eec019c0c055f22c880364d5
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2022-07-11 10:20:54 +0700

    fix address

--- routers/userAddressRoute.js
+++ routers/userAddressRoute.js
@@ -5,149 +5,169 @@ import UserAddress from "../models/UserAddressModel";
 const userAddressRoute = express.Router();
 
 userAddressRoute.get("/", Auth, async (req, res) => {
-    try {
-        const result = await UserAddress.findOne({ "user.user_id": req.user.id });
-        res.json({ status: 'ok', data: result });
-    } catch (error) {
-        res.json(error);
-    }
+  try {
+    const result = await UserAddress.findOne({ "user_id": req.user.id });
+    res.json({ status: "ok", data: result });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 userAddressRoute.get("/:id", Auth, async (req, res) => {
-    try {
-        let { id } = req.params;
-        if (!id) {
-            id = req.user.id;
-        }
-        const result = await UserAddress.findOne({ "user.user_id": req.user.id });
-        res.json(result);
-    } catch (error) {
-        res.json(error);
+  try {
+    let { id } = req.params;
+    if (!id) {
+      id = req.user.id;
     }
+    const result = await UserAddress.findOne({ "user_id": req.user.id });
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 userAddressRoute.get("/edit-address/:id", Auth, async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        if (!id) {
-            return res.status(404).json({ message: "ไม่พบข้อมูล" });
-        }
-
-        const result = await UserAddress.findOne({ _id: id });
-        res.json(result);
-    } catch (error) {
-        res.json(error);
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
     }
+
+    const result = await UserAddress.findOne({ _id: id });
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 userAddressRoute.post("/delete-address", Auth, async (req, res) => {
-    try {
-        const { id, sub_id } = req.fields;
+  try {
+    const { id, sub_id } = req.fields;
 
-        if (!id) {
-            return res.status(404).json({ message: "ไม่พบข้อมูล" });
-        }
-
-        const result = await UserAddress.findOneAndUpdate({ _id: id }, {
-            $pull: { userAddress: { _id: sub_id } }
-        });
-
-        res.json({ status: 'ok', message: 'Delete Successfully!' });
-    } catch (error) {
-        res.json({ status: 'nok', message: error });
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
     }
+
+    const result = await UserAddress.findOneAndUpdate(
+      { _id: id },
+      {
+        $pull: { userAddress: { _id: sub_id } },
+      }
+    );
+
+    res.json({ status: "ok", message: "Delete Successfully!" });
+  } catch (error) {
+    res.json({ status: "nok", message: error });
+  }
 });
 
 userAddressRoute.put("/:id", Auth, async (req, res) => {
-    try {
-        let { data } = req.fields;
-        data = JSON.parse(data);
-        const { id } = req.params;
+  try {
+    let { data } = req.fields;
+    data = JSON.parse(data);
+    const { id } = req.params;
 
-        if (!id) {
-            return res.status(404).json({ message: "ไม่พบข้อมูล" });
-        }
-
-        const result = await UserAddress.findOneAndUpdate({ _id: id, "userAddress._id": data._id }, {
-            $set: { 'userAddress.$': data }
-        });
-
-        res.json({ status: 'ok', message: 'Update Successfully!' });
-    } catch (error) {
-        res.json({ status: 'nok', message: error });
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
     }
+
+    const result = await UserAddress.findOneAndUpdate(
+      { _id: id, "userAddress._id": data._id },
+      {
+        $set: { "userAddress.$": data },
+      }
+    );
+
+    res.json({ status: "ok", message: "Update Successfully!" });
+  } catch (error) {
+    res.json({ status: "nok", message: error });
+  }
 });
 
 userAddressRoute.post("/activete-address", Auth, async (req, res) => {
-    try {
-        const { id, sub_id } = req.fields;
+  try {
+    const { id, sub_id } = req.fields;
 
-        if (!id) {
-            return res.status(404).json({ message: "ไม่พบข้อมูล" });
-        }
-
-        const result = await UserAddress.findOneAndUpdate({ _id: id }, {
-            $set: { "addressShipping": sub_id },
-        });
-
-        res.json({ status: 'ok', message: 'Activete Successfully!' });
-    } catch (error) {
-        res.json({ status: 'nok', message: error });
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
     }
+
+    const result = await UserAddress.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: { addressShipping: sub_id },
+      }
+    );
+
+    res.json({ status: "ok", message: "Activete Successfully!" });
+  } catch (error) {
+    res.json({ status: "nok", message: error });
+  }
 });
 
 userAddressRoute.post("/default-address", Auth, async (req, res) => {
-    try {
-        const { id, sub_id, def_id } = req.fields;
+  try {
+    const { id, sub_id, def_id } = req.fields;
 
-        if (!id) {
-            return res.status(404).json({ message: "ไม่พบข้อมูล" });
-        }
-
-        if (def_id && def_id == 1) {
-            const result = await UserAddress.findOneAndUpdate({ _id: id }, {
-                $set: { "addressOrderDefault": sub_id },
-            });
-        } else if (def_id && def_id == 2) {
-            const result = await UserAddress.findOneAndUpdate({ _id: id }, {
-                $set: { "addressKatecheckDefault": sub_id },
-            });
-        } else if (def_id && def_id == 3) {
-            const result = await UserAddress.findOneAndUpdate({ _id: id }, {
-                $set: { "addressSellWithUsDefault": sub_id },
-            });
-        } else if (def_id && def_id == 4) {
-            const result = await UserAddress.findOneAndUpdate({ _id: id }, {
-                $set: { "addressReturnProductDefault": sub_id },
-            });
-        }
-
-        res.json({ status: 'ok', message: 'Activete Successfully!' });
-    } catch (error) {
-        res.json({ status: 'nok', message: error });
+    if (!id) {
+      return res.status(404).json({ message: "ไม่พบข้อมูล" });
     }
+
+    if (def_id && def_id == 1) {
+      const result = await UserAddress.findOneAndUpdate(
+        { _id: id },
+        {
+          $set: { addressOrderDefault: sub_id },
+        }
+      );
+    } else if (def_id && def_id == 2) {
+      const result = await UserAddress.findOneAndUpdate(
+        { _id: id },
+        {
+          $set: { addressKatecheckDefault: sub_id },
+        }
+      );
+    } else if (def_id && def_id == 3) {
+      const result = await UserAddress.findOneAndUpdate(
+        { _id: id },
+        {
+          $set: { addressSellWithUsDefault: sub_id },
+        }
+      );
+    } else if (def_id && def_id == 4) {
+      const result = await UserAddress.findOneAndUpdate(
+        { _id: id },
+        {
+          $set: { addressReturnProductDefault: sub_id },
+        }
+      );
+    }
+
+    res.json({ status: "ok", message: "Activete Successfully!" });
+  } catch (error) {
+    res.json({ status: "nok", message: error });
+  }
 });
 
-
 userAddressRoute.post("/", async (req, res) => {
-    try {
-        let { data } = req.fields;
-        data = JSON.parse(data);
+  try {
+    let { data } = req.fields;
+    data = JSON.parse(data);
 
-        const checkUser = await UserAddress.findOne({ user_id: data.user_id });
-        if (checkUser) {
-            //update data or update address
-            await UserAddress.findOneAndUpdate({ user_id: data.user_id }, data);
-        } else {
-            //insert 
-            await UserAddress.create(data);
-        }
-
-        res.json({ status: 'ok', message: 'Insert Data Successfully!' });
-    } catch (error) {
-        res.json({ status: 'nok', message: error });
+    const checkUser = await UserAddress.findOne({ user_id: data.user_id });
+    if (checkUser) {
+      //update data or update address
+      await UserAddress.findOneAndUpdate({ user_id: data.user_id }, data);
+    } else {
+      //insert
+      await UserAddress.create(data);
     }
+
+    res.json({ status: "ok", message: "Insert Data Successfully!" });
+  } catch (error) {
+    res.json({ status: "nok", message: error });
+  }
 });
 
 export default userAddressRoute;
